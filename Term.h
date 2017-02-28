9ca46da6a608b7d6f5ba1bc4f65adfeb2a7f9beb
chenchenpeng <pengy@william.jewell.edu>
2017-02-28 14:23:09 -0600

    Update Term.h
    Revise some of the documentations

--- Term.h
+++ Term.h
@@ -15,16 +15,16 @@ public:
 		exponent = 0;
 	}
 
-	/**Construct a new Term 
-	@param c
-	@param e
+	/**Construct a new Term
+	@param c (coefficient)
+	@param e (exponent)
 	*/
-	Term(double c, double e) {
+	Term(int c, int e) {
 		coefficient = c;
 		exponent = e;
 	}
 
-	//Overload the + operator too add the terms with the same exponents
+	//Overload the + operator to add the Terms with the same exponents
 	const Term& operator + (const Term& other) {
 		if (this->exponent != other.exponent) {
 			throw std::exception("Cannot add two terms with different exponents!");
@@ -36,65 +36,69 @@ public:
 	}
 
 	/**Overload the < operator to compare the exponents of the Terms
-	   See whether the left Term's exponent is smaller than the right 
-	   Term's exponent
+	See whether the left Term's exponent is smaller than the right
+	Term's exponent
 	*/
 	bool operator < (const Term& other) {
 		return this->exponent < other.exponent;
 	}
 
 	/**Overload the > operator to compare the exponents of the Terms
-	   See whether the left Term's exponent is bigger than the right 
-	   Term's exponent
+	See whether the left Term's exponent is bigger than the right
+	Term's exponent
 	*/
 	bool operator > (const Term& other) {
 		return this->exponent > other.exponent;
 	}
 
 	/**Overload the >= operator to compare the exponents of the Terms
-	   See whether the left Term's exponent is bigger than or equal to the 
-	   right Term's exponent
+	See whether the left Term's exponent is bigger than or equal to the
+	right Term's exponent
 	*/
 	bool operator >= (const Term& other) {
 		return this->exponent >= other.exponent;
 	}
-	
+
 	/**Overload the <= operator to compare the exponents of the Terms
-	   See whether the left Term's exponent is less than or equal to the
-	   right Term's exponent
+	See whether the left Term's exponent is less than or equal to the
+	right Term's exponent
 	*/
 	bool operator <= (const Term& other) {
 		return this->exponent <= other.exponent;
 	}
 
 	/**Overload the == operator to compare the exponents of the Terms
-	   See whether the left Term's exponent is equal to the right Term's 
-	   exponent
+	See whether the left Term's exponent is equal to the right Term's
+	exponent
 	*/
 	bool operator == (const Term& other) {
 		return this->exponent == other.exponent;
 	}
 
 	/**Overload the != operator to compare the exponents of the Terms
-	See whether the left Term's exponent is not equal to the right 
+	See whether the left Term's exponent is not equal to the right
 	Term's exponent
 	*/
 	bool operator != (const Term& other) {
 		return this->exponent != other.exponent;
 	}
-	//returns the value in coefficient
+
+	//Return the value in coefficient
 	int getCoef() {
 		return this->coefficient;
 	}
-	//updates the value in coefficient
+
+	//Update the value in coefficient
 	void setCoef(int coef) {
 		this->coefficient = coef;
 	}
-	//updates the value in exponent
+
+	//Update the value in exponent
 	void setExpn(int expn) {
 		this->exponent = expn;
 	}
-	//returns the value in exponent
+
+	//Return the value in exponent
 	int getExpn() {
 		return this->exponent;
 	}
