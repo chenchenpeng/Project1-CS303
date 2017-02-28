592f3e79f5d1516ee8e696c92c56e70548924264
chenchenpeng <pengy@william.jewell.edu>
2017-02-28 17:28:19 -0600

    Update Polynomial.h

--- Polynomial.h
+++ Polynomial.h
@@ -9,43 +9,43 @@
 
 class Polynomial {
 private:
-	//a list of class term which contains expon and coeff ints, defaults to empty list
+	//A list of class term which contains expon and coeff ints, defaults to empty list
 	std::list<Term> T;
 
 public:
-	//inserts new node into list in order decending by expn
+	//Insert new node into list in order decending by expn
 	void orderedInsert(const int expn, const int coef) {
 		Term newNode;
 		//int compareExpn, coeffecient;
 		newNode = Term(coef, expn);
-		//check for empty list and push node to front if empty
+		//Check for empty list and push node to front if empty
 		if (T.empty()) {
 			T.push_front(newNode);
 		}
 		else {
-			//traverse list using an iterator to find ordered insert placement
+			//Traverse list using an iterator to find ordered insert placement
 			std::list<Term>::iterator& itr = T.begin();
 			while (itr != T.end()) {
-				//get value of current nodes expn for comparison, if they are equal, add coef to the nodes coef
+				//Get value of current nodes expn for comparison, if they are equal, add coef to the nodes coef
 				if (itr->getExpn() == expn) {
 					itr->setCoef(itr->getCoef() + coef);
-					//if the new coef = 0, erase the node
+					//If the new coef = 0, erase the node
 					if (itr->getCoef() == 0) {
 						itr = T.erase(itr);
 					}
 					return;
 				}
-				//if nodes expn is greater than expn, iterator to the next node
+				//If nodes expn is greater than expn, iterator to the next node
 				else if (itr->getExpn() > expn) {
 					++itr;
-					//if at the end of the list, expn is less than all expn in the list, 
-					//push a new node to the end of the list
+					//If at the end of the list, expn is less than all expn in the list, 
+					//Push a new node to the end of the list
 					if (itr == T.end()) {
 						T.push_back(newNode);
 						return;
 					}
 				}
-				else if (itr->getCoef() == 0) //avoid keeping track of terms with zero coefficient
+				else if (itr->getCoef() == 0) //Avoid keeping track of terms with zero coefficient
 				{
 					++itr;
 				}
@@ -57,21 +57,21 @@ public:
 			}
 		}
 	}
-	//reads expression into a dll list of term type with int coeff and int expon
+	//Read expression into a dll list of term type with int coeff and int expon
 	void ReadFromString(std::string exprInput) {
 		std::string term, searchForSecondStr;
 		int index2ndSign, i, j, expnLocation, expnOffset;
 		int expn, coef;
 		expn = 0;
 		coef = 0;
-		//convert str it upper for X comparison ref:cpluplus.com
+		//Convert str it upper for X comparison ref:cpluplus.com
 		std::transform(exprInput.begin(), exprInput.end(), exprInput.begin(), ::toupper);
 		while (!exprInput.empty()) {
 			i = 0;
 			j = 0;
-			/**ignore leading + or -*/
+			//Ignore leading + or -
 			i = exprInput.find_first_of("+-");
-			//if leading + or minus truncate off str for second + or - search
+			//If leading + or minus truncate off str for second + or - search
 			if (i == 0) {
 				i = 1;
 				searchForSecondStr = exprInput.substr(1);
@@ -81,23 +81,25 @@ public:
 				searchForSecondStr = exprInput;
 			}
 			if (!searchForSecondStr.empty()) {
-				/**find next + or -*/
+				//find next + or -
 				j = searchForSecondStr.find_first_of("+-");
 				if (j >= 0) {
-					//split string into everything before the + or minus (term)
-					//and everything from the + or minus on for next iteration
+					/**Split string into everything before the + or minus (term)
+					and everything from the + or minus on for next iteration
+					*/
 					index2ndSign = j + i;
 					term = exprInput.substr(0, index2ndSign);
 					exprInput = exprInput.substr(index2ndSign);
 				}
-				//case for when only one term is in the string, also empties
-				//string to exit while loop
+				/**Case for when only one term is in the string, also empties
+				string to exit while loop
+				*/
 				else {
 					term = exprInput;
 					exprInput = "";
 				}
 			}
-			/**search for X in the term string. If X is not found, convert the whole term to int*/
+			//search for X in the term string. If X is not found, convert the whole term to int
 			i = term.find_first_of('X');
 			expnOffset = 0;
 			expnLocation = term.find_first_of('^');
@@ -106,47 +108,52 @@ public:
 				//^ found skip two characters to find expn
 				if (expnLocation >= 0) {
 					expnOffset = i + 2;
-					//convert expn string to int ref:cplusplus.com
+					//Convert expn string to int ref:cplusplus.com
 					expn = stoi(term.substr(expnOffset));
-				} else {
+				}
+				else {
 					expn = 1;//^ not found, value of X is 1
 				}
-				//check for term begins with -X the coef is -1
+				//Check for term begins with -X the coef is -1
 				if (term.substr(0, 1) == "-" && i == 1) {
 					coef = -1;
-				} else if (term.substr(0, 1) == "+" && i == 1) { // check for term begins with +X the coef is 1
-					coef = 1;
-				} else if (i == 0) { //X is first char in term then coef is 1
-					coef = 1;
-				} else {
-					coef = stoi(term.substr(0, i));//no special cases, convert everything before X to int ref:cplusplus.com
 				}
-			} else { //X not found, expn is 0, convert whole term to int ref:cplusplus.com
+				else if (term.substr(0, 1) == "+" && i == 1) { // Check for term begins with +X the coef is 1
+					coef = 1;
+				}
+				else if (i == 0) { //X is first char in term then coef is 1
+					coef = 1;
+				}
+				else {
+					coef = stoi(term.substr(0, i));//No special cases, convert everything before X to int ref:cplusplus.com
+				}
+			}
+			else { //X not found, expn is 0, convert whole term to int ref:cplusplus.com
 				expn = 0;
 				coef = stoi(term);
 			}
-			//send expn and coef to the list for ordered insert
+			//Send expn and coef to the list for ordered insert
 			orderedInsert(expn, coef);
 		}
 	}
 
-	//adds two polynomial lists into the list that calls add
-	//uses references to two other polyniomial lists
+	//Add two polynomial lists into the list that calls add
+	//Use references to two other polyniomial lists
 	void Add(Polynomial& Poly1, Polynomial& Poly2) {
 		std::list<Term>::iterator ptr1 = Poly1.T.begin(), ptr2 = Poly2.T.begin();
-		//insert 1st list a new list using ordered insert
+		//Insert 1st list a new list using ordered insert
 		while (ptr1 != Poly1.T.end()) {
 			orderedInsert(ptr1->getExpn(), ptr1->getCoef());
 			++ptr1;
 		}
-		//insert 2nd list into the new list using ordered insert
+		//Insert 2nd list into the new list using ordered insert
 		while (ptr2 != Poly2.T.end()) {
 			orderedInsert(ptr2->getExpn(), ptr2->getCoef());
 			++ptr2;
 		}
 	}
 
-	//outputs the list as a stringed friendly polynomial
+	//Output the list as a stringed friendly polynomial
 	void Output() {
 		std::list<Term>::iterator ptr = T.begin();
 		std::string output;
@@ -159,19 +166,19 @@ public:
 				if (coef != 1 && coef != -1) {
 					output = output + std::to_string(coef);
 				}
-				//add - to the output string and ignor coef -1
+				//Add - to the output string and ignore the digit of coef -1
 				if (coef == -1) {
 					output = output + "-";
 				}
 				//Add expn to the output string
 				expn = ptr->getExpn();
-				if (expn < 0 || expn > 1) { // add X^ if the expn greater than 1 or less than 0
+				if (expn < 0 || expn > 1) { // Add X^ if the expn greater than 1 or less than 0
 					output = output + "X^" + std::to_string(expn);
 				}
-				else if (expn == 1) { //just add X if the expn = 1
+				else if (expn == 1) { //Add X if the expn = 1
 					output = output + "X";
 				}
-				ptr++; //advance ptr, check to see if there is another term, if so add + to output for positive coeffs
+				ptr++; //Advance ptr, check to see if there is another term, if so add + to output for positive coeffs
 				if (ptr != T.end()) {
 					nextcoef = ptr->getCoef();
 					if (nextcoef > 0) {
@@ -179,23 +186,23 @@ public:
 					}
 				}
 			}
-			//verify that output has no leading +
+			//Verify that output has no leading +
 			if (output.substr(0, 1) == "+") {
 				output = output.substr(1);
 			}
 		}
-		else { // ouput is empty, set to 0
+		else { // Ouput is empty, set to 0
 			output = "0";
 		}
-		std::cout << output << std::endl; //send output to cout 
+		std::cout << output << std::endl; //Send output to cout 
 	}
 
-	//check for empty list
+	//Check for empty list
 	bool emptyPoly() {
 		return T.empty();
 	}
 
-	//clean up a linked list
+	//Clean up a linked list
 	void removeAll()
 	{
 		T.clear();
